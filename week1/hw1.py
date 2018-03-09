40986181021f5494014577030ef81b362b6ac7fd
Furkanguven1 <36991895+Furkanguven1@users.noreply.github.com>
2018-03-09 14:48:31 +0300

    Update hw1.py

--- week1/hw1.py
+++ week1/hw1.py
@@ -1,1 +1,19 @@
-print ("Hello")
+dongu = int(input("Dongu sinirini belirleyiniz")
+for i in range(0,dongu) :
+    birincisayi = int(input("Birinci sayıyı giriniz:"))
+    ikincisayi = int(input("İkinci sayiyi giriniz:"))
+    islem = input("Yapacağınız işlemi seçiniz(+,-,/,*):")
+    if islem == "+" :
+        print (birincisayi+ikincisayi)
+    elif islem == "-" :
+        print (birincisayi-ikincisayi)
+    elif islem == "*" :
+        print (birincisayi*ikincisayi)
+    elif (islem == "/") & (ikincisayi==0):
+        print ("Gecerli bir sayi girisi yapiniz")
+    elif islem =="/" :
+        print (birincisayi/ikincisayi)
+    elif islem == "exit":
+        break
+    else:
+        print("Lütfen belirtilen operatörlerden birini giriniz."
